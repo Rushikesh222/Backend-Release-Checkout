dbbcf20a282438cdf449dabbfb3f6772c4b1109b
Rushikesh Shirsat <90494892+Rushikesh222@users.noreply.github.com>
2026-04-26 12:07:02 +0530

    main - Update the file path

--- src/api/release/index.js
+++ src/api/release/index.js
@@ -1,5 +1,5 @@
-import connectDB from "../../lib/db.js";
-import Release from "../../models/Release.js";
+import connectDB from "../../config/db.js";
+import Release from "../../models/release.model.js";
 
 export default async function handler(req, res) {
   await connectDB();
